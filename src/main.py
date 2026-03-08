980ab9cb22f48e226e12c6d5b908377749930f14
AAperghis <albert@aperghis.nl>
2026-03-08 17:49:54 +0100

    added CORS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,19 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from webhook import router as webhook_router
 from endpoints.banaan import router as banaan_router
 from endpoints.roster import router as roster_router
 
 app = FastAPI(root_path=os.getenv("ROOT_PATH", ""))
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.getenv("CORS_ORIGINS", "*").split(","),
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(webhook_router)
 app.include_router(banaan_router)
 app.include_router(roster_router)
