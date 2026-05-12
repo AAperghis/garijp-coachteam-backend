d171131d7c5cd44465397b137ff19d59069428de
AAperghis <albert@aperghis.nl>
2026-05-12 15:55:53 +0200

    chore: add run_both.sh dev script

---
+++ scripts/run_both.sh
@@ -0,0 +1,6 @@
+cd /home/aaperghis/software/apps/garijp-coachteam/garijp-coachteam-backend
+source /home/aaperghis/software/apps/garijp-coachteam/venv/bin/activate
+uv run python3 -m src.backend.main
+
+cd /home/aaperghis/software/apps/garijp-coachteam/garijp-coachteam-frontend
+npm run dev
